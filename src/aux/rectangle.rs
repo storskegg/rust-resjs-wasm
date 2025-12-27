64acd09fefe144bc1420d611d95233b968e6dda5
Liam Conrad <liam@w4pho.net>
2025-12-27 12:39:27 -0600

    resolve bugs in aux::rectangle

--- src/aux/rectangle.rs
+++ src/aux/rectangle.rs
@@ -8,23 +8,22 @@ pub struct Rectangle {
     height: f64,
 }
 
-pub fn new_rectangle(x: f64, y: f64, width: f64, height: f64) -> Rectangle {
-    Rectangle { x, y, width, height }
-}
-
 impl Rectangle {
-    pub fn mirror(&self, width: f64) -> Rectangle {
-        new_res_rectangle(width - self.x - self.width, self.y, self.width, self.height)
+    pub fn new(x: f64, y: f64, width: f64, height: f64) -> Self {
+        Self { x, y, width, height }
+    }
+    pub fn mirror(&self, width: f64) -> Self {
+        Rectangle::new(width - self.x - self.width, self.y, self.width, self.height)
     }
 
-    pub fn intersect(&self, other: &Rectangle) -> Rectangle {
+    pub fn intersect(&self, other: &Rectangle) -> Self {
         let x_left = self.x.max(other.x);
         let x_right = (self.x + self.width).min(other.x + other.width);
         let y_top = self.y.max(other.y);
         let y_bottom = (self.y + self.height).min(other.y + other.height);
         let w = x_right.max(x_left) - x_left;
         let h = y_bottom.max(y_top) - y_top;
-        new_res_rectangle(x_left, y_top, w, h)
+        Rectangle::new(x_left, y_top, w, h)
     }
 
     pub fn intersects(&self, other: &Rectangle) -> bool {
@@ -36,19 +35,19 @@ impl Rectangle {
     }
 
     pub fn chop_start_h(&self, x: f64) -> Rectangle {
-        new_res_rectangle(self.x, self.y, x - self.x, self.height)
+        Rectangle::new(self.x, self.y, x - self.x, self.height)
     }
 
     pub fn chop_end_h(&self, x: f64) -> Rectangle {
-        new_res_rectangle(x, self.y, self.width - x + self.x, self.height)
+        Rectangle::new(x, self.y, self.width - x + self.x, self.height)
     }
 
     pub fn chop_start_v(&self, y: f64) -> Rectangle {
-        new_res_rectangle(self.x, self.y, self.width, y - self.y)
+        Rectangle::new(self.x, self.y, self.width, y - self.y)
     }
 
     pub fn chop_end_v(&self, y: f64) -> Rectangle {
-        new_res_rectangle(self.x, y, self.width, self.height - y + self.y)
+        Rectangle::new(self.x, y, self.width, self.height - y + self.y)
     }
 
     pub fn chop_pattern(&self, pattern: &str) -> Rectangle {
@@ -56,19 +55,19 @@ impl Rectangle {
 
         for c in pattern.chars() {
             if c == 't' {
-                result = new_res_rectangle(self.x, self.y, self.width, self.height/2.0)
+                result = Rectangle::new(self.x, self.y, self.width, self.height/2.0)
             }
 
             if c == 'b' {
-                result = new_res_rectangle(self.x, self.y + self.height/2.0, self.width, self.height/2.0)
+                result = Rectangle::new(self.x, self.y + self.height/2.0, self.width, self.height/2.0)
             }
 
             if c == 's' {
-                result = new_res_rectangle(self.x, self.y, self.width/2.0, self.height)
+                result = Rectangle::new(self.x, self.y, self.width/2.0, self.height)
             }
 
             if c == 'e' {
-                result = new_res_rectangle(self.x + self.width/2.0, self.y, self.width/2.0, self.height)
+                result = Rectangle::new(self.x + self.width/2.0, self.y, self.width/2.0, self.height)
             }
         }
 
@@ -80,6 +79,6 @@ impl Rectangle {
         let vertical_surplus = self.height - other.height;
         let x = self.x + horizontal_surplus /2.0 - other.x;
         let y = self.y + vertical_surplus /2.0 - other.y;
-        new_res_rectangle(x, y, other.width, other.height)
+        Rectangle::new(x, y, other.width, other.height)
     }
 }
