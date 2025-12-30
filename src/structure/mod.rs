12ddaadb38aae76375c2cdc0c36d961121473237
Liam Conrad <liam@w4pho.net>
2025-12-30 16:04:31 -0500

    structure: port fragment Part 1 of...

--- src/structure/mod.rs
+++ src/structure/mod.rs
@@ -1,1 +1,2 @@
 mod globals; pub use globals::*;
+mod fragment; pub use fragment::*;
--- src/structure/res_struct.js
+++ src/structure/res_struct.js
@@ -2,70 +2,7 @@
 
 // Data structure.
 
-function ResFragment(args) {
-	if (args.l) {
-		var argList = args.l;
-		var switchs = args.sw;
-		var hiero = args.h;
-		this.direction = null;
-		this.size = null;
-		for (var i = 0; i < argList.length; i++) {
-			var arg = argList[i];
-			if (arg.is("hlr") || arg.is("hrl") ||
-					arg.is("vlr") || arg.is("vrl"))
-				this.direction = arg.getLhs();
-			else if (arg.hasLhs("size") && arg.hasRhsNonzeroReal())
-				this.size = arg.getRhs();
-		}
-		this.switchs = switchs;
-		this.hiero = hiero;
-		this.propagateBack();
-		this.propagate();
-	} else {
-		this.direction = args.direction;
-		this.size = args.size;
-		this.switchs = args.switchs;
-		this.hiero = args.hiero;
-		this.propagateBack();
-		this.propagate();
-	}
-}
-ResFragment.prototype.headerString =
-function() {
-	var args = [];
-	if (this.direction !== null)
-		args.push(this.direction);
-	if (this.size !== null)
-		args.push("size=" + ResArg.realStr(this.size));
-	var s = ResArg.argsStr(args);
-	return s;
-};
-ResFragment.prototype.toString =
-function() {
-	var s = this.headerString();
-	s += this.switchs.toString();
-	if (this.hiero !== null) 
-		s += this.hiero.toString();
-	return s;
-};
-ResFragment.prototype.propagateBack =
-function() {
-	if (this.hiero !== null) {
-		var sw = this.hiero.propagateBack();
-		this.switchs = this.switchs.join(sw);
-	}
-};
-ResFragment.prototype.propagate =
-function() {
-	this.globals = new ResGlobals(this.direction, this.size);
-	this.globals = this.switchs.update(this.globals);
-	if (this.hiero !== null)
-		this.globals = this.hiero.propagate(this.globals, this.globals.direction);
-};
-ResFragment.prototype.namedGlyphs =
-function() {
-	return this.hiero === null ? [] : this.hiero.namedGlyphs();
-};
+
 
 function ResHieroglyphic(args) {
 	this.groups = [];
