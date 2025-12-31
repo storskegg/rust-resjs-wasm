6ebed114564d21cb48945e7e98cb0a42bf5c573a
Liam Conrad <liam@w4pho.net>
2025-12-31 10:28:46 -0500

    NOTE: fragment comment re: future deletion

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -1,1 +1,4 @@
-mod fragment; pub use fragment::*;
+// mod fragment; pub use fragment::*;
+
+// This file will be removed once the format methods in res_format.js are added to their respective
+// classes
